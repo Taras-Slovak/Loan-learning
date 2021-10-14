deb509649240b746d8d8d90701473b4505deb19b
Taras Slovak <slovak.taras@gmail.com>
2021-10-14 17:57:17 +0300

    add slider for modules.html

--- src/js/main.js
+++ src/js/main.js
@@ -12,33 +12,40 @@ window.addEventListener('DOMContentLoaded', () => {
     });
     slider.render();
 
-    const showUpSlider  = new MiniSlider({
-        container:'.showup__content-slider', 
+    const modulePageSlider = new MainSlider({
+        container: '.moduleapp',
+        btns: '.next'
+    });
+
+    modulePageSlider.render();
+
+    const showUpSlider = new MiniSlider({
+        container: '.showup__content-slider',
         prev: '.showup__prev',
         next: '.showup__next',
-        activeClass:'card-active',
+        activeClass: 'card-active',
         animate: true
     });
 
     showUpSlider.init();
 
     const modulesSlider = new MiniSlider({
-        container:'.modules__content-slider', 
+        container: '.modules__content-slider',
         prev: '.modules__info-btns .slick-prev',
         next: '.modules__info-btns .slick-next',
         activeClass: 'card-active',
         animate: true,
-        autoplay:true
+        autoplay: true
     });
 
     modulesSlider.init();
 
     const feedsSlider = new MiniSlider({
-        container:'.feed__slider', 
+        container: '.feed__slider',
         prev: '.feed__slider .slick-prev',
         next: '.feed__slider .slick-next',
         activeClass: 'feed__item-active'
-        
+
     });
 
     feedsSlider.init();
@@ -46,7 +53,7 @@ window.addEventListener('DOMContentLoaded', () => {
     const player = new VideoPlayer('.showup .play', '.overlay');
     player.init();
 
-    new Difference('.officerold','.officernew','.officer__card-item').init();
+    new Difference('.officerold', '.officernew', '.officer__card-item').init();
 
     new Form('.form').init();
 

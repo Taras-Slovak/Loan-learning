13b7c61bf73c0967d2937331ff84f19547c1fd37
Taras Slovak <slovak.taras@gmail.com>
2021-10-15 21:11:49 +0300

    show info

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ import MiniSlider from "./modules/slider/slider-mini";
 import VideoPlayer from "./modules/playVideo";
 import Difference from "./modules/difference";
 import Form from "./modules/form";
+import ShowInfo from "./modules/showinfo";
 
 window.addEventListener('DOMContentLoaded', () => {
 
@@ -52,10 +53,12 @@ window.addEventListener('DOMContentLoaded', () => {
 
     new VideoPlayer('.showup .play', '.overlay').init();
 
-    new VideoPlayer('.module__video-item .play','.overlay').init();
+    new VideoPlayer('.module__video-item .play', '.overlay').init();
 
     new Difference('.officerold', '.officernew', '.officer__card-item').init();
 
     new Form('.form').init();
+
+    new ShowInfo('.plus__content').init();
 
 });

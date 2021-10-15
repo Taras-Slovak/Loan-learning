3b50146b4c437098d9bfd1dc8ec949f754fc6ef5
Taras Slovak <slovak.taras@gmail.com>
2021-10-15 20:16:01 +0300

    upd playVideo.js

--- src/js/main.js
+++ src/js/main.js
@@ -50,8 +50,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
     feedsSlider.init();
 
-    const player = new VideoPlayer('.showup .play', '.overlay');
-    player.init();
+    new VideoPlayer('.showup .play', '.overlay').init();
+
+    new VideoPlayer('.module__video-item .play','.overlay').init();
 
     new Difference('.officerold', '.officernew', '.officer__card-item').init();
 
